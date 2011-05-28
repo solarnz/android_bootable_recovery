6d34f2a0a714155dd976e7121669e7e481368e76
Chris Trotman <chris@solarnz.net>
2011-05-28 20:25:42 +1200

    Implemented advanced backup, being able to choose what partitions to backup

--- nandroid.h
+++ nandroid.h
@@ -3,6 +3,7 @@
 
 int nandroid_main(int argc, char** argv);
 int nandroid_backup(const char* backup_path);
+int nandroid_advanced_backup(const char* backup_path, int boot, int recovery, int system, int data, int cache, int sdext, int wimax);
 int nandroid_restore(const char* backup_path, int restore_boot, int restore_system, int restore_data, int restore_cache, int restore_sdext, int restore_wimax);
 
 #endif
