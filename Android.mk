5baf61ded288199ba711869a5820e9271f8c0835
Chris Trotman <chris@solarnz.net>
2012-01-13 15:59:10 +1300

    Switch to EOS logo.
    Change-Id: I8827e80b7f134adcf49a24a022f3004c0b3fd9e1

--- Android.mk
+++ Android.mk
@@ -31,7 +31,7 @@ ifdef I_AM_KOUSH
 RECOVERY_NAME := ClockworkMod Recovery
 LOCAL_CFLAGS += -DI_AM_KOUSH
 else
-RECOVERY_NAME := CWM-based Recovery
+RECOVERY_NAME := EOS Recovery (CWM based)
 endif
 
 RECOVERY_VERSION := $(RECOVERY_NAME) v5.5.0.4
